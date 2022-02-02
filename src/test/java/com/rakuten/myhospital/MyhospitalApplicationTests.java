d17fc15ab04346b139bd5006c71de54dbaed0499
rahul.a.sahu <rahul.a.sahu@rakuten.com>
2022-02-02 14:13:38 +0530

    1.0.0

---
+++ src/test/java/com/rakuten/myhospital/MyhospitalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rakuten.myhospital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyhospitalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
